84b6187ac3218e5fa28bea4a1c06b76873fb731b
Suzukeh <suzukeh@gmail.com>
2021-01-01 20:58:51 +0900

    Add word list to replace.

--- NIKUEConverter/nikueconverter.js
+++ NIKUEConverter/nikueconverter.js
@@ -1,14 +1,14 @@
-function f(n){
+function niku(n) {
   let cs = n.childNodes;
-  for(let i = 0; i < cs.length; i++){
+  for (let i = 0; i < cs.length; i++) {
     let c = cs[i];
-    if(c.nodeType == Node.TEXT_NODE){
-      c.textContent = c.textContent.replace(/MIKUEC/g, "NIKUEC").replace(/mikuec/g, "nikuec").replace(/みくえっく/g, "にくえっく").replace(/ライブ/g, "焼き肉");
+    if (c.nodeType == Node.TEXT_NODE) {
+      c.textContent = c.textContent.replace(/MIKUEC/g, "NIKUEC").replace(/mikuec/g, "nikuec").replace(/みくえっく/g, "にくえっく").replace(/ライブ/g, "焼き肉").replace(/ステージ/g, "プレート").replace(/曲目/g, "皿目").replace(/楽曲/g, "お肉").replace(/曲/g, "肉");
     }
-    else{
-      f(c);
+    else {
+      niku(c);
     }
   }
 }
 
-f(document.body);
+niku(document.body);
